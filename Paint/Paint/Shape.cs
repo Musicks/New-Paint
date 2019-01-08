88fa4153e626d82be2f19cc495f06d30b3e34176
Sagar Khadka <breakdowns.blasts@gmail.com>
2019-01-08 14:23:01 +0545

    Created a new Shape class file

---
+++ Paint/Paint/Shape.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+
+namespace shapes
+{
+    public abstract class Shapes
+    {
+        public string name;
+
+        protected Pen red = new Pen(Color.Red);
+        protected Pen blue = new Pen(Color.Blue);
+        protected Pen yellow = new Pen(Color.Yellow);
+        protected Pen black = new Pen(Color.Black);
+
+        protected System.Drawing.SolidBrush fillRed = new System.Drawing.SolidBrush(Color.Red);
+        protected System.Drawing.SolidBrush fillBlue = new System.Drawing.SolidBrush(Color.Blue);
+        protected System.Drawing.SolidBrush fillYellow = new System.Drawing.SolidBrush(Color.Yellow);
+
+        public abstract double area();
+        protected abstract void fillBackground(SolidBrush s);
+    }
+}
