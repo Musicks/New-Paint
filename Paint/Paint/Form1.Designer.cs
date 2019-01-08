4e940f314d2dd0c752ec791e471b60ff33daea46
Sagar Khadka <breakdowns.blasts@gmail.com>
2019-01-08 17:35:27 +0545

    Created a new circle, rectangle and triangle class file

--- Paint/Paint/Form1.Designer.cs
+++ Paint/Paint/Form1.Designer.cs
@@ -28,20 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(266, 372);
+            this.textBox1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(307, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(325, 372);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(664, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Paint/Paint/Form1.cs
+++ Paint/Paint/Form1.cs
@@ -16,5 +16,60 @@ namespace Paint
         {
             InitializeComponent();
         }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String command = textBox1.Text;
+            string[] syntax = command.Split('\n');
+            int i = syntax.Length;
+            int count = 0;
+            while (count < i)
+            {
+
+                string[] text = syntax[count].Split(' ');
+                if (text[0].ToUpper().Equals("DRAW"))
+                {
+
+                    string[] param = text[2].Split(',');
+                    if (text[1].ToUpper().Equals("RECTANGLE"))
+                    {
+                        int parameter1 = Convert.ToInt32(param[0]);
+                        int parameter2 = Convert.ToInt32(param[1]);
+                        int parameter3 = Convert.ToInt32(param[2]);
+                        int parameter4 = Convert.ToInt32(param[3]);
+                        MessageBox.Show("Draw rectangle with parameter" + parameter1 + "and" + parameter2);
+                    }
+                    if (text[1].ToUpper().Equals("TRIANGLE"))
+                    {
+                        int parameter1 = Convert.ToInt32(param[0]);
+                        int parameter2 = Convert.ToInt32(param[1]);
+                        int parameter3 = Convert.ToInt32(param[2]);
+                        int parameter4 = Convert.ToInt32(param[3]);
+                        MessageBox.Show("Draw triangle with parameter" + parameter1 + "and" + parameter2);
+                    }
+                    if (text[1].ToUpper().Equals("CIRCLE"))
+                    {
+                        int parameter1 = Convert.ToInt32(param[0]);
+                        int parameter2 = Convert.ToInt32(param[1]);
+                        int parameter3 = Convert.ToInt32(param[2]);
+                        int parameter4 = Convert.ToInt32(param[3]);
+                        MessageBox.Show("Draw circle with parameter" + parameter1 + "and" + parameter2);
+                    }
+                    if (text[1].ToUpper().Equals("POLYGON"))
+                    {
+                        int parameter1 = Convert.ToInt32(param[0]);
+                        int parameter2 = Convert.ToInt32(param[1]);
+                        int parameter3 = Convert.ToInt32(param[2]);
+                        int parameter4 = Convert.ToInt32(param[3]);
+                        MessageBox.Show("Draw polygon with parameter" + parameter1 + "and" + parameter2);
+                    }
+                }
+                count++;
+            }
+        }
     }
 }
